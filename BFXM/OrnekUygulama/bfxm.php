7d2d9dbcb8e54683909192635076727a15fa2cf1
Volkan Oransoy <voransoy@gmail.com>
2015-03-24 23:52:41 +0200

    Bfxm örnek php uygulaması eklendi.

---
+++ BFXM/OrnekUygulama/bfxm.php
@@ -0,0 +1,73 @@
+<?php
+	$step = isset($_POST['step']) ? $_POST['step'] : 1;
+	$uuid = isset($_POST['uuid']) ? $_POST['uuid'] : false;
+	$caller = isset($_POST['caller']) ? $_POST['caller'] : false;
+	$callee = isset($_POST['callee']) ? $_POST['callee'] : false;
+	$returnvar = isset($_POST['returnvar']) ? $_POST['returnvar'] : false;
+
+	if(!($uuid || $caller || $callee))
+	{
+		header('Content-Type: application/json');
+		echo json_encode(['error'=>'parameters missing']);
+		die();
+	}
+	//$db = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8', 'username', 'password');
+
+
+
+	switch ($step) {
+		case 1:
+			$cevap = [
+				"bfml"=>["version"=>1],
+				"seq"=>[[
+					"action"=>"play",
+					"args"=>["url"=> "http://bfxmdemo.bulutfon.com/demosesler/demo-hosgeldiniz.mp3"]
+				],
+				[
+					"action" => "gather",
+					"args" => [
+				        "min_digits"  => 1,
+				        "max_digits"  => 1,
+				        "max_attempts"  => 3,
+				        "ask"  => "http://bfxmdemo.bulutfon.com/demosesler/numara-tuslayiniz.mp3",
+				        "play_on_error"  => "http://bfxmdemo.bulutfon.com/demosesler/hatali-giris.mp3",
+				        "variable_name"  => "returnvar",
+			      ]
+
+				]],
+
+			];
+			header('Content-Type: application/json');
+			echo json_encode($cevap);
+			break;
+		case 2:
+			$cevap = [
+				"bfml"=>["version"=>1],
+				"seq"=>[[
+					"action"=>"play",
+					"args"=>["url"=> "http://bfxmdemo.bulutfon.com/demosesler/tesekkurler.mp3"]
+				],
+				[
+					"action" => "hangup"
+
+				]],
+
+			];
+			header('Content-Type: application/json');
+			echo json_encode($cevap);
+
+			$mesaj = "Son arayan " . $caller . " " . $returnvar . " tuşladı.\n";
+
+			$file = "call_log.txt";
+			/**
+			 * Write to file
+			 */
+			$fh = fopen($file, 'w');
+			fwrite($fh,$mesaj);
+			fclose($fh);
+
+			break;
+		default:
+			die("Uygulamadan cik");
+			break;
+	}
---
+++ BFXM/OrnekUygulama/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BFXMDEMO</title>
+	<style>
+		html,body {
+			height: 100%;
+		}
+		iframe {
+			height: 90%;
+		}
+	</style>
+</head>
+<body>
+	<h1>BFXM Demo Uygulaması</h1>
+	<p>Bulutfon demo uygulamasına hoşgeldiniz. Test için 0850 885 0 223 numaralı telefonu arayabilrisiniz. Uygulamanın kaynak kodunu <a href="https://github.com/bulutfon/documents/tree/master/BFXM">Github</a> üzerinden görebilrisiniz.</p>
+
+	<h3>Son test edenler</h3>
+	<iframe src="call_log.txt" frameborder="0" style="width:100%;height:100%;"></iframe>
+</body>
+</html>
